5922cc83c703eb77a1d2025fa1425bfa13ab9e8d
Денис Коваленко <103291152+TwinDen0@users.noreply.github.com>
2024-05-05 22:41:08 +1000

    122121

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+	error_reporting(0);
+	$db_host = 'localhost';
+	$db_user = 'root';
+	$db_password = '';
+	$db_name = 'users';
+	
+	$link = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+?>
--- foo.php
+++ foo.php
@@ -1,6 +1,29 @@
 <?php
-$text = "aaaaa";
-$text = $_POST['data'];
+    error_reporting(0);
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_password = '';
+    $db_name = 'quizball';
 
-error_log($text);
+    $link = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+    // Проверка подключения
+    if (!$link) {
+        die("Ошибка подключения: " . mysqli_connect_error());
+    }
+
+    $text = "aaaaa";
+    if(isset($_POST['data'])){
+        $text = mysqli_real_escape_string($link, $_POST['data']); // экранирование данных перед использованием в запросе
+    }
+
+    $sql = "INSERT INTO users (name, phone, info) VALUES ('$text', 121, 'aaaa')"; // Убрано поле id и значение NULL
+    if ($result = mysqli_query($link, $sql)) {
+        echo "Данные успешно добавлены в базу данных";
+    } else {
+        echo "Ошибка при выполнении запроса: " . mysqli_error($link);
+    }
+
+    // Закрываем соединение с базой данных
+    mysqli_close($link);
 ?>
